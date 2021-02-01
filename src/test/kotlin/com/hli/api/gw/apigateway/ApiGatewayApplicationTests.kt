9bea60480a525e0c600fb9c9aac0e2b05358df93
siha80 <siha80@gmail.com>
2021-02-01 14:49:30 +0900

    project base for api gateway

---
+++ src/test/kotlin/com/hli/api/gw/apigateway/ApiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hli.api.gw.apigateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
